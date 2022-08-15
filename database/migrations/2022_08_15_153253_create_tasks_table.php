b3c9387194083a249f7346e7f1c4bee4fa95610f
BoengGit <edukasiasqor@gmail.com>
2022-08-15 22:53:32 +0700

    belajar laravel 8 parsinta | 19

---
+++ database/migrations/2022_08_15_153253_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id(); // bigInterger - Auto Increment = Primary Key
+            $table->string('list', 191); // default 255
+            $table->timestamps();
+            // created_at
+            // updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
