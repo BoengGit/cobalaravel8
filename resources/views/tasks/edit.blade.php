30f8fcd1b16afa5fceb384fa3ea6fe5f9286a3c3
BoengGit <edukasiasqor@gmail.com>
2022-08-20 13:05:02 +0700

    belajar laravel 8 parsinta | 28

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -1,10 +1,18 @@
 <x-app-layout>
-    <h1>Tasks</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <h1>Tasks</h1>
 
-    <form action="/tasks/{{ $task->id }}" method="post">
-        @method('put')
-        @csrf
-        <input type="text" name="list" value="{{ $task->list }}" placeholder="The name of the task">
-        <button type="submit">Update</button>
-    </form>
+                <form action="{{ route('tasks.update', $task->id) }}" method="post">
+                    @method('put')
+                    @csrf
+                    <div class="mb-2">
+                        <input class="form-control" type="text" name="list" value="{{ $task->list }}" placeholder="The name of the task">
+                    </div>
+                    <button class="btn btn-primary" type="submit">Update</button>
+                </form>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,7 +5,7 @@
                 <div class="card">
                     <div class="card-header">Create new Task</div>
                     <div class="card-body">
-                        <form action="/tasks" method="post" class="d-flex">
+                        <form action="{{ route('tasks.store') }}" method="post" class="d-flex">
                         @csrf
                         <input class="form-control me-2" type="text" name="list" placeholder="The name of the task">
                         <button class="btn btn-primary" type="submit">Add</button>
@@ -20,8 +20,8 @@
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     {{ $index + 1 }} - {{ $task->list }}
                     <div class="d-flex"> 
-                        <a class="btn btn-primary me-2" href="/tasks/{{ $task->id }}/edit">edit</a>
-                        <form action="/tasks/{{ $task->id }}" method="post">
+                        <a class="btn btn-primary me-2" href="{{ route('tasks.edit', $task->id) }}">edit</a>
+                        <form action="{{ route('tasks.destroy', $task->id) }}" method="post">
                             @csrf
                             @method('delete')
                             <button class="btn btn-danger" type="submit">Delete</button>
