1ac6918255ca92367ca49884fa78c6dc93fbf130
BoengGit <edukasiasqor@gmail.com>
2022-08-20 16:27:02 +0700

    belajar laravel 8 parsinta | 29

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -13,26 +14,27 @@ class UsersSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        collect([
-            [
-                'name' => 'Bajang Riau',
-                'email' => 'bajang@gmail.com',
-                'password' => bcrypt('password'),
-                'email_verified_At' => now(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'name' => 'Gilang Riau',
-                'email' => 'gilang@gmail.com',
-                'password' => bcrypt('password'),
-                'email_verified_At' => now(),
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-        ])->each(function ($user) {
-            DB::table('users')->insert($user);
-        });
+    {   
+        User::factory()->count(10)->create();
+        // collect([
+        //     [
+        //         'name' => 'Bajang Riau',
+        //         'email' => 'bajang@gmail.com',
+        //         'password' => bcrypt('password'),
+        //         'email_verified_At' => now(),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ],
+        //     [
+        //         'name' => 'Gilang Riau',
+        //         'email' => 'gilang@gmail.com',
+        //         'password' => bcrypt('password'),
+        //         'email_verified_At' => now(),
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ],
+        // ])->each(function ($user) {
+        //     DB::table('users')->insert($user);
+        // });
     }
 }
