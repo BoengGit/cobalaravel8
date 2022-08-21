3c9feb0db416add668d87155ad6b5f9524f939fd
BoengGit <edukasiasqor@gmail.com>
2022-08-21 13:01:25 +0700

    belajar laravel 8 parsinta | 35

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,18 @@
 <x-app-layout>
-    <h1>Home</h1>
+    <div class="container row">
+        <div class="col-md-6">
+            @if (session()->has('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ session()->get('success') }}
+                </div>
+            @endif
+            <div class="card">
+                <div class="card-header">Welcome to my site</div>
+                <div class="card-body">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique deserunt tempora ad quisquam, a fugit iste necessitatibus nemo corporis non, cum vero officiis quo et earum minima eius. Perferendis, a.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi eveniet tempora eius natus ullam harum minus quo, nesciunt animi cum distinctio ipsum delectus, temporibus optio? Nihil repellendus sunt aliquid illum?</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
