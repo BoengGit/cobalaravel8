c3d76c79098a46bf0d923cfbba18770259bbb81f
BoengGit <edukasiasqor@gmail.com>
2022-08-21 19:52:57 +0700

    belajar laravel 8 parsinta | 36

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistrationRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,28 +14,10 @@ class RegistrationController extends Controller
         return view('auth.register');
     }
 
-    public function store(Request $request)
+    public function store(RegistrationRequest $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'min:3'],
-            'username' => ['required', 'unique:users', 'alpha_num', 'min:3', 'max:25'],
-            'email' => ['required','unique:users', 'email'],
-            'password' => ['required', 'min:8'],
-        ]);
+        User::create($request->all());
 
-        // $user = User::where('email', $request->email)->orWhere('username', $request->username)->first();
-        // if ($user) {
-        //     dd('user sudah ada');
-        // }
-
-        User::create([
-            'name' => $request->name,
-            'username' => $request->username,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        session()->flash('success', 'Thank you, you are now registered.');
-        return redirect('/');
+        return redirect('/')->with('success', 'Thank you, you are now registered.');
     }
 }
