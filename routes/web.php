e01cd9b8a6344a8558767643c299b8ef8f00b2d0
BoengGit <edukasiasqor@gmail.com>
2022-08-23 13:13:12 +0700

    belajar laravel 8 parsinta | 38

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProfileInformationController;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\TaskController;
@@ -13,7 +14,10 @@ Route::get('/', HomeController::class);
 Route::resource('tasks', TaskController::class);
 
 Route::get('register', [RegistrationController::class, 'create'])->name('register');
-Route::post('register', [RegistrationController::class, 'store'])->name('register');
+Route::post('register', [RegistrationController::class, 'store']);
+
+Route::get('login', [LoginController::class, 'create'])->name('login');
+Route::post('login', [LoginController::class, 'store']);
 
 Route::get('users', [UserController::class, 'index']);
 Route::get('users/{user:username}', [UserController::class, 'show'])->name('users.show');
